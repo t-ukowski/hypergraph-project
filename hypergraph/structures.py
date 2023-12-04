7700b51b656184c52798213a96655494f3d22088
lys1k <lysikadrianna@gmail.com>
2023-12-04 20:03:27 +0100

    add edge class

--- hypergraph/structures.py
+++ hypergraph/structures.py
@@ -14,7 +14,14 @@ class Node:
         self.h = h
         self.label = label
         self.level = level
-        self.id = next(self._id_counter)
+        self.id = next(self._id_counter) # remove if not needed
+
+class Edge:
+    def __init__(self, v1: Node, v2: Node, B: int, R: int):
+        self.v1 = v1
+        self.v2 = v2
+        self.B = B
+        self.R = R
 
 class Graph:
     def __init__(self):
@@ -29,13 +36,13 @@ class Graph:
     def add_node(self, node: Node):
         self.G.add_node(node, node=node)
 
-    def add_edge(self, v1: Node, v2: Node):
+    def add_edge(self, v1: Node, v2: Node, B: int, R: int):
         if v1 not in self:
             raise ValueError(f"{v1} not in graph")
         if v2 not in self:
             raise ValueError(f"{v2} not in graph")
 
-        self.G.add_edge(v1, v2)
+        self.G.add_edge(v1, v2, B=B, R=R)
 
     def get_nodes(self):
         return self.G.nodes
@@ -44,7 +51,7 @@ class Graph:
         return self.G.number_of_nodes()
 
     def get_edges(self):
-        return self.G.edges
+        return self.G.edges(data=True)
 
     def get_number_of_edges(self):
         return self.G.number_of_edges()
@@ -65,26 +72,36 @@ class Graph:
     def visualize(self):
         pos = {node: (node.x, node.y) for node in self.G.nodes}
         labels = {node: node.label for node in self.G.nodes}
-        nx.draw(graph.G, pos, with_labels=True, labels=labels, node_color='lightblue', node_size=500, font_size=12,
+        nx.draw(graph.G, pos, with_labels=True, labels=labels, node_color='lightblue', node_size=400, font_size=10,
                 edge_color='gray')
         plt.show()
 
 
 
-
-
-node1 = Node(1, 3, 3, "a")
-node2 = Node(1, 1, 1, "b")
-node3 = Node(4, 2, 3, "c")
+node1 = Node(1, 3, 3, "one", 1)
+node2 = Node(1, 1, 1, "two", 1)
+node3 = Node(4, 2, 3, "three", 1)
 
 graph = Graph()
+
 graph.add_node(node1)
 graph.add_node(node2)
 graph.add_node(node3)
 
+graph.add_edge(node1, node2, 1, 2)
+graph.add_edge(node2, node3, 3,4)
 
-graph.add_edge(node1, node2)
-graph.add_edge(node2, node3)
+nodes = graph.get_nodes()
+first_node = next(iter(nodes))
+label = first_node.label
+
+print(label)
+
+edges = graph.get_edges()
+first_edge = next(iter(edges))
+label = first_edge[2]['B']
+
+print(label)
 
 
 graph.visualize()
