8578c022d65fd8b85e93f8797886d1e97de60962
patcet <patrykcet2000@gmail.com>
2023-12-06 18:51:28 +0100

    Add QNode, ENode and hyperedges

--- hypergraph/structures.py
+++ hypergraph/structures.py
@@ -16,6 +16,24 @@ class Node:
         self.label = label
         self.id = next(self._id_counter) # TODO: remove if not needed
 
+class QNode:
+    _id_counter = count()
+    def __init__(self, x: float, y: float, h=0, label=""):
+        self.x = x
+        self.y = y
+        self.h = h
+        self.label = label
+        self.id = next(self._id_counter) # TODO: remove if not needed
+
+class ENode:
+    _id_counter = count()
+    def __init__(self, x: float, y: float, h=0, label=""):
+        self.x = x
+        self.y = y
+        self.h = h
+        self.label = label
+        self.id = next(self._id_counter) # TODO: remove if not needed
+
 # TODO: B and R should be int or boolean ?
 class Edge:
     def __init__(self, v1: Node, v2: Node, B=0, R=0):
@@ -25,6 +43,7 @@ class Edge:
         self.R = R
 
 class Graph:
+
     def __init__(self):
         self.G = nx.Graph()
 
@@ -37,13 +56,40 @@ class Graph:
     def add_node(self, node: Node):
         self.G.add_node(node, node=node)
 
+    def add_q_node(self, n1,n2,n3,n4):
+        node = QNode(
+            x = (n1.x + n2.x + n3.x + n4.x) / 4,
+            y = (n1.y + n2.y + n3.y + n4.y) / 4,
+            label = "Q"
+        )
+        self.G.add_node(node)
+        self.add_hyperedge(n1, node)
+        self.add_hyperedge(n2, node)
+        self.add_hyperedge(n3, node)
+        self.add_hyperedge(n4, node)
+        return node
+
+    def add_hyperedge(self, v1: QNode, v2: QNode):
+        if v1 not in self:
+            raise ValueError(f"{v1} not in graph")
+        if v2 not in self:
+            raise ValueError(f"{v2} not in graph")
+        self.G.add_edge(v1, v2)
+
     def add_edge(self, v1: Node, v2: Node, B=0, R=0):
+        node = ENode(
+            x = (v1.x + v2.x) / 2,
+            y = (v1.y + v2.y) / 2,
+            label = "E"
+        )
         if v1 not in self:
             raise ValueError(f"{v1} not in graph")
         if v2 not in self:
             raise ValueError(f"{v2} not in graph")
 
         self.G.add_edge(v1, v2, B=B, R=R)
+        self.G.add_node(node)
+        return node
 
     def get_nodes(self):
         return self.G.nodes
@@ -74,8 +120,16 @@ class Graph:
 
     def visualize(self):
         pos = {node: (node.x, node.y) for node in self.G.nodes}
-        labels = {node: node.label for node in self.G.nodes}
-        nx.draw(self.G, pos, with_labels=True, labels=labels, node_color='lightblue', node_size=400, font_size=10,
+        labels = {node: node.label + " " + str(node.id) for node in self.G.nodes}
+        color_map = []
+        for node in self.G:
+            if node.label == "Q":
+                color_map.append('lightgreen')
+            elif node.label == "E":
+                color_map.append("lightgrey")
+            else:
+                color_map.append('lightblue')
+        nx.draw(self.G, pos=pos, with_labels=True, labels=labels, node_color=color_map, node_size=400, font_size=10,
                 edge_color='gray')
         plt.show()
 
--- hypergraph/tmp.py
+++ hypergraph/tmp.py
@@ -2,9 +2,9 @@ from hypergraph.P1 import P1
 from hypergraph.structures import Node, Graph
 
 node1 = Node(0, 0, 3, "one")
-node2 = Node(2,0, 1, "two")
-node3 = Node(2, 2, 3, "three")
-node4 = Node(0, 2, 3, "four")
+node2 = Node(5,0, 1, "two")
+node3 = Node(4, 2, 3, "three")
+node4 = Node(1, 2, 3, "four")
 
 
 graph = Graph()
@@ -13,12 +13,12 @@ graph.add_node(node1)
 graph.add_node(node2)
 graph.add_node(node3)
 graph.add_node(node4)
+qnode_1 = graph.add_q_node(node1,node2,node3,node4)
 
-
-graph.add_edge(node1, node2, 1, 2)
-graph.add_edge(node2, node3, 3,4)
-graph.add_edge(node3, node4, 3,4)
-graph.add_edge(node4, node1, 3,4)
+enode_1 = graph.add_edge(node1, node2, 1, 2)
+enode_2 = graph.add_edge(node2, node3, 3,4)
+enode_3 = graph.add_edge(node3, node4, 3,4)
+enode_4 = graph.add_edge(node4, node1, 3,4)
 
 
 
