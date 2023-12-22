4bc722eaf99570d86f3353dd4dc786a6a0e237eb
Wolokin <fnikol22@gmail.com>
2023-12-22 15:43:00 +0100

    Zakomentowana wizualizacja testów P13

--- hypergraph/tests/test_p13.py
+++ hypergraph/tests/test_p13.py
@@ -1,9 +1,11 @@
 import unittest
-from hypergraph.structures import Node, Graph
+
 from hypergraph.productions.p13 import P13
+from hypergraph.structures import Graph, Node
 
 
 class TestP13Production(unittest.TestCase):
+
     def setUpCompleteGraph(self):
         # Ustawienie dla kompletnego grafu (używane w teście test_p13_production_applies)
         self.prop = P13()
@@ -14,16 +16,12 @@ class TestP13Production(unittest.TestCase):
         node = Node(5, 2, 0)
         self.prop.nodes.append(node)
         self.prop.graph.add_node(node)  # Dodanie nowego węzła
-        self.prop.graph.add_edge(
-            self.prop.nodes[4], node
-        )  # Dodanie krawędzi między nowym węzłem a węzłem 4
+        self.prop.graph.add_edge(self.prop.nodes[4], node)  # Dodanie krawędzi między nowym węzłem a węzłem 4
 
     def setUpIncompleteGraphWithMissingVertex(self):
         # Ustawienie dla niekompletnego grafu (używane w teście test_p1_production_does_not_apply_because_missing_vertex)
         self.setUpCompleteGraph()
-        self.prop.graph.remove_node(
-            self.prop.nodes[3]
-        )  # Celowo pomijamy czwarty węzeł i jego krawędzie
+        self.prop.graph.remove_node(self.prop.nodes[3])  # Celowo pomijamy czwarty węzeł i jego krawędzie
         # Celowo pomijamy czwarty węzeł i jego krawędzie
 
     def setUpIncompleteGraphWithMissingEdge(self):
@@ -32,9 +30,7 @@ class TestP13Production(unittest.TestCase):
         self.prop.graph.remove_edge(
             self.prop.nodes_in_order[0],
             self.prop.nodes_in_order[1],
-            self.prop.enodes_dict[
-                (self.prop.nodes_in_order[0], self.prop.nodes_in_order[1])
-            ],
+            self.prop.enodes_dict[(self.prop.nodes_in_order[0], self.prop.nodes_in_order[1])],
         )  # Celowo pomijamy krawędź między node1 a node2
         # Celowo pomijamy krawędź między node3 a node4
 
@@ -45,6 +41,7 @@ class TestP13Production(unittest.TestCase):
 
     def test_p13_production_applies(self):
         self.setUpCompleteGraph()
+        # self.prop.graph.visualize()
         prod = P13()
         results = prod.search_for_subgraphs(self.prop.graph)
 
@@ -60,9 +57,12 @@ class TestP13Production(unittest.TestCase):
         # 2. Sprawdź liczbę krawędzi
         expected_num_edges = 24
         self.assertEqual(self.prop.graph.get_number_of_edges(), expected_num_edges)
+        # self.prop.graph.visualize()
 
     def test_p13_production_applies_to_larger_graph(self):
+
         self.setUpLargerCompleteGraph()
+        # self.prop.graph.visualize()
         prod = P13()
         results = prod.search_for_subgraphs(self.prop.graph)
         for subgraph in results:
@@ -77,30 +77,37 @@ class TestP13Production(unittest.TestCase):
         # 2. Sprawdź liczbę krawędzi
         expected_num_edges = 25  # 4 oryginalne krawędzie są podzielone na 8, a do węzła centralnego dodane są 4 nowe krawędzie
         self.assertEqual(self.prop.graph.get_number_of_edges(), expected_num_edges)
+        # self.prop.graph.visualize()
 
     def test_p13_production_does_not_apply_because_missing_vertex(self):
         self.setUpIncompleteGraphWithMissingVertex()  # Ustawienie niekompletnego grafu dla tego testu
+        # self.prop.graph.visualize()
         prod = P13()
         results = list(prod.search_for_subgraphs(self.prop.graph))
 
         # Sprawdź, czy nie znaleziono podgrafów (produkcja nie powinna być stosowana)
         self.assertEqual(len(results), 0)
+        # self.prop.graph.visualize()
 
     def test_p13_production_does_not_apply_because_missing_edge(self):
         self.setUpIncompleteGraphWithMissingEdge()  # Ustawienie grafu z brakującą krawędzią dla tego testu
+        # self.prop.graph.visualize()
         prod = P13()
         results = list(prod.search_for_subgraphs(self.prop.graph))
 
         # Sprawdź, czy nie znaleziono podgrafów (produkcja nie powinna być stosowana z brakującą krawędzią)
         self.assertEqual(len(results), 0)
+        # self.prop.graph.visualize()
 
     def test_p13_production_does_not_apply_because_incorrect_R(self):
         self.setUpGraphWithIncorrectR()  # Ustawienie grafu z niepoprawną wartością R dla tego testu
+        # self.prop.graph.visualize()
         prod = P13()
         results = list(prod.search_for_subgraphs(self.prop.graph))
 
         # Sprawdź, czy nie znaleziono podgrafów (produkcja nie powinna być stosowana z niepoprawną wartością R)
         self.assertEqual(len(results), 0)
+        # self.prop.graph.visualize()
 
 
 if __name__ == "__main__":
